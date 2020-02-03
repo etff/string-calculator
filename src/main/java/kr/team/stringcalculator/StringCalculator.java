fe434ef071973d33687b67fb74fc10a1523f21eb
Rebwon <msolo021015@naver.com>
2020-02-03 15:44:52 +0900

    feat: StringCalculator를 요구사항에 맞게 구현.

--- src/main/java/kr/team/stringcalculator/StringCalculator.java
+++ src/main/java/kr/team/stringcalculator/StringCalculator.java
@@ -1,8 +1,31 @@
 package kr.team.stringcalculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     public int add(String input) {
-        // TODO 구현.
-        return 0;
+        if(input == null || input.isEmpty()){
+            return 0;
+        }
+        int sum = 0;
+        if(input.contains(",")){
+            String[] split = input.split(",|:");
+            for(int i=0; i<split.length; i++){
+                sum += Integer.parseInt(split[i]);
+            }
+            return sum;
+        }else{
+            Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
+            if(matcher.find()){
+                String customDelimeter = matcher.group(1);
+                String[] split = matcher.group(2).split(customDelimeter);
+                for(int i=0; i<split.length; i++){
+                    sum += Integer.parseInt(split[i]);
+                }
+                return sum;
+            }
+        }
+        return Integer.parseInt(input);
     }
 }
