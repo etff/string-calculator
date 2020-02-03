420d99da2747ef2992740ff5274009feed2249f0
Rebwon <msolo021015@naver.com>
2020-02-03 17:16:27 +0900

    refactor: 각 메서드 별 기능에 맞게 세분화 작업.

--- src/main/java/kr/team/stringcalculator/StringCalculator.java
+++ src/main/java/kr/team/stringcalculator/StringCalculator.java
@@ -8,17 +8,17 @@ public class StringCalculator {
         if(isBlack(input)){
             return 0;
         }
-        return sum(split(input));
+        return sum(toInts(split(input)));
     }
 
     private boolean isBlack(String input){
         return input == null || input.isEmpty();
     }
 
-    private int sum(String[] numbers){
+    private int sum(int[] numbers){
         int sum = 0;
-        for(int i=0; i<numbers.length; i++){
-            sum += Integer.parseInt(numbers[i]);
+        for(int number : numbers){
+            sum += number;
         }
         return sum;
     }
@@ -27,9 +27,17 @@ public class StringCalculator {
         Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
         if(matcher.find()){
             String customDelimeter = matcher.group(1);
-            String[] split = matcher.group(2).split(customDelimeter);
-            return split;
+            String[] values = matcher.group(2).split(customDelimeter);
+            return values;
         }
         return input.split(",|:");
+    }
+
+    private int[] toInts(String[] values){
+        int[] numbers = new int[values.length];
+        for(int i=0; i<values.length; i++){
+            numbers[i] += Integer.parseInt(values[i]);
+        }
+        return numbers;
     }
 }
