a4d08601530a23e91d64db9de219d0f0d9ec36d8
Rebwon <msolo021015@naver.com>
2020-02-03 14:25:14 +0900

    initial commit

---
+++ src/main/java/kr/team/stringcalculator/StringCalculator.java
@@ -0,0 +1,8 @@
+package kr.team.stringcalculator;
+
+public class StringCalculator {
+    public int add(String input) {
+        // TODO 구현.
+        return 0;
+    }
+}
---
+++ src/test/java/kr/team/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,46 @@
+package kr.team.stringcalculator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCalculatorTest {
+    private StringCalculator cal;
+
+    @Before
+    public void setup(){
+        cal = new StringCalculator();
+    }
+
+    @Test
+    public void add_null_또는_empty() throws Exception {
+        assertThat(cal.add("")).isEqualTo(0);
+        assertThat(cal.add(null)).isEqualTo(0);
+    }
+
+    @Test
+    public void add_숫자하나() throws Exception {
+        assertThat(cal.add("1")).isEqualTo(1);
+    }
+
+    @Test
+    public void add_쉼표구분자() throws Exception {
+        assertThat(cal.add("1,3")).isEqualTo(4);
+    }
+
+    @Test
+    public void add_쉼표_콜론_구분자() throws Exception {
+        assertThat(cal.add("1,3:2")).isEqualTo(6);
+    }
+
+    @Test
+    public void add_커스텀구분자() throws Exception {
+        assertThat(cal.add("//;\n1;2;3")).isEqualTo(6);
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void add_음수를_입력() throws Exception {
+        cal.add("-2;2");
+    }
+}
