92e2719d86a1502f2245a846bfac9618d9203465
김동현 <dhyc9395@gmail.com>
2020-02-03 16:01:42 +0900

    dosc: passed all of TestCase commit

--- src/main/java/kr/team/stringcalculator/StringCalculator.java
+++ src/main/java/kr/team/stringcalculator/StringCalculator.java
@@ -1,8 +1,22 @@
 package kr.team.stringcalculator;
 
+import java.util.Optional;
+
 public class StringCalculator {
     public int add(String input) {
         // TODO 구현.
+        if (input.contains("-")) System.exit(0);
+
+        Optional<String> value = Optional.ofNullable(input);
+        if (!value.isPresent()) return 0;
+
+        int result = 0;
+        char[] chars = input.toCharArray();
+        for (char aChar : chars) {
+            if ('0' <= aChar && aChar <= '9') {
+                result += Integer.parseInt(String.valueOf(aChar));
+            }
+        }
         return 0;
     }
 }
