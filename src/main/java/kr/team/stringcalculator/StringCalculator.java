9fe9250ed595411b2b2d21a04d25cf7e4092c6c7
김동현 <dhyc9395@gmail.com>
2020-02-03 21:20:50 +0900

    refactor: 예외처리 추가, 최종 리펙토링 완료

--- src/main/java/kr/team/stringcalculator/StringCalculator.java
+++ src/main/java/kr/team/stringcalculator/StringCalculator.java
@@ -6,15 +6,18 @@ import java.util.List;
 import java.util.Optional;
 
 public class StringCalculator {
+
     public int add(String input) {
         if (!checkNull(input)) {
             return sum(checkNumber(input));
         }
         return 0;
     }
-    private int sum (int[] nums) {
+    
+    private int sum(int[] nums) {
         return Arrays.stream(nums).sum();
     }
+
     private int[] checkNumber(String input) {
         List<String> nums = new ArrayList<>();
         char[] chars = input.toCharArray();
@@ -25,8 +28,14 @@ public class StringCalculator {
         }
         return nums.stream().mapToInt(Integer::parseInt).toArray();
     }
+
     private boolean checkNull(String input) {
         Optional<String> value = Optional.ofNullable(input);
-        return (!value.isPresent() || value == null) ? true : false;
+        if (!value.isPresent()) {
+            return true;
+        } else if (value.get().contains("-")) {
+            throw new RuntimeException();
+        }
+        return false;
     }
 }
