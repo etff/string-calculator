21d0439aa47f6de28914ac436841d095a6136d78
김동현 <dhyc9395@gmail.com>
2020-02-03 17:53:26 +0900

    refactor: sum 부분과 불필요한 static , add 메소드 짧게 refactoring

--- src/main/java/kr/team/stringcalculator/StringCalculator.java
+++ src/main/java/kr/team/stringcalculator/StringCalculator.java
@@ -2,24 +2,22 @@ package kr.team.stringcalculator;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 public class StringCalculator {
     public int add(String input) {
-
         if (!checkNull(input)) {
-            int sum = 0;
-            int[] nums = checkNumber(input);
-            for (int num : nums) {
-                sum += num;
-            }
-            return sum;
+            return sum(checkNumber(input));
         }
         return 0;
     }
 
-    static int[] checkNumber(String input) {
+    private int sum (int[] nums) {
+        return Arrays.stream(nums).sum();
+    }
+    private int[] checkNumber(String input) {
         List<String> nums = new ArrayList<>();
         char[] chars = input.toCharArray();
         for (char c : chars) {
@@ -30,7 +28,7 @@ public class StringCalculator {
         return nums.stream().mapToInt(Integer::parseInt).toArray();
     }
 
-    static boolean checkNull(String input) {
+    private boolean checkNull(String input) {
         Optional<String> value = Optional.ofNullable(input);
         if (!value.isPresent() || value == null) return true;
         return false;
