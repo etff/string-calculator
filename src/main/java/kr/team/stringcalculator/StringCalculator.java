69bb72292958dbbcadbb2569117cfafdb176d35b
etff <wish2rich@gmail.com>
2020-05-03 18:24:18 +0900

    refactor(calculator): null check method 추출

--- src/main/java/kr/team/stringcalculator/StringCalculator.java
+++ src/main/java/kr/team/stringcalculator/StringCalculator.java
@@ -7,12 +7,16 @@ import java.util.List;
 
 public class StringCalculator {
     public int add(String input) {
-        if (input == null || input.trim().isEmpty()) {
+        if (checkIsEmpty(input)) {
             return 0;
         }
         List<String> splitText = NumberParser.splitText(input);
         PostiveNumbers postiveNumbers = new PostiveNumbers(NumberParser.convertToNumbers(splitText));
 
         return postiveNumbers.sum();
+    }
+
+    private boolean checkIsEmpty(String input) {
+        return input == null || input.trim().isEmpty();
     }
 }
